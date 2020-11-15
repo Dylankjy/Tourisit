aa700fd77357f49b9d42c15ac91daa09e8adf8fb
AC <alexchien09@gmail.com>
2020-11-15 14:57:48 +0800

    Mde edits

--- routes.py
+++ routes.py
@@ -9,9 +9,11 @@ app = Flask(__name__,
 def home():
     return render_template('index.html')
 
+
 @app.route('/id/login')
 def login():
     return render_template('login.html')
+
 
 @app.route('/id/signup')
 def signup():
