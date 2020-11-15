003fdbfab3d77b829655a6665d557e20bae95b83
Dylan Kok <tenkotofu@gmail.com>
2020-11-15 14:46:00 +0800

    Update file structure
    Co-Authored-By: Alex C <59523055+Alexc09@users.noreply.github.com>

---
+++ routes.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, 
+            static_folder='public',
+            template_folder='templates')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/id/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/id/signup')
+def signup():
+    return render_template('signup.html')
