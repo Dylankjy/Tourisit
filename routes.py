e3aff503e0ab4043e203864346aa328755e43eda
AC <alexchien09@gmail.com>
2020-11-20 21:34:57 +0800

    added stuff

--- routes.py
+++ routes.py
@@ -8,21 +8,18 @@ app = Flask(__name__,
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
-    info = {}
-    return render_template('index.html', info=info)
+    return render_template('index.html')
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    global info
     if (request.method == 'POST') and (request.form['submit_button'] == 'yes'):
         name = request.form['username']
         status = request.form['submit_button']
-        info = {'name': name, 'status': status}
-        return render_template('index.html', info=info)
+        return render_template('index.html')
 
     info = {}
-    return render_template('login.html', info=info)
+    return render_template('login.html')
 
 
 @app.route('/signup')
