90e10cc4894814a2e3ae7518ed69225bf32c4403
root <alexchien09@gmail.com>
2020-11-23 00:27:11 +0000

    Made changes to setup.py

--- routes.py
+++ routes.py
@@ -375,7 +375,8 @@ def signup():
 
 
 # Run app
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
 
 
 # This edits the tour_name of the 4th listing
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 apt update
 sudo apt install python3-pip
 pip3 install -r requirements.txt
+python3 routes.py
