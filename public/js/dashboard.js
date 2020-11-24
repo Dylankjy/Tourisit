40a57f141080d45d2def1c11540556e4a47937c5
Dylan Kok <tenkotofu@gmail.com>
2020-11-24 23:05:29 +0800

    Add data for charts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,73 +1,91 @@
-var ctx = document.getElementById('expensesChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var incomeChart = document.getElementById('incomeChart').getContext('2d');
+var incomeChartData = new Chart(incomeChart, {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['July', 'August', 'September', 'October', 'November', 'December', 'January', 'February'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Gross Income',
+            data: [638, 640, 780, 644, 743, 768],
+            backgroundColor: 'rgba(0, 145, 189, 0.2)',
+            borderWidth: 0
+        }, {
+            label: 'Net Earnings',
+            data: [533, 556, 693, 568, 678, 696],
+            backgroundColor: 'rgba(0, 189, 148, 0.2)',
+            borderWidth: 0
+        }, {
+            label: 'Net Earnings (prediction)',
+            data: [null, null, null, null, null, 768, 820, 783],
+            backgroundColor: pattern.draw('dash', 'rgba(0, 0, 0, 0.08)'),
+            borderWidth: 0
         }]
     },
     options: {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
-                }
+                    beginAtZero: false,
+                    callback: function(value, index, values) {
+                        // Convert the number to a string and splite the string every 3 charaters from the end
+                        value = value.toString();
+                        value = value.split(/(?=(?:...)*$)/);
+            
+                        // Convert the array to a string and format the output
+                        value = value.join('.');
+                        return '$' + value;
+                    }
+                },
             }]
-        }
+        },
+        elements: {
+            line: {
+                cubicInterpolationMode: 'monotone'
+            }
+        },
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    return data['labels'][tooltipItem['index']] + ': ' + '$' + data['datasets'][0]['data'][tooltipItem['index']];
+                }
+            }
+        },
     }
 });
 
-var ctx = document.getElementById('refundChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+var csatChart = document.getElementById('csatChart').getContext('2d');
+var csatChartData = new Chart(csatChart, {
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Satisfaction rate (%)',
+            data: [98.6, 97.1, 98.4, 90.2, 86.4, 96.4],
+            backgroundColor: 'rgba(224, 34, 85, 0.2)',
+            borderWidth: 0
         }]
     },
     options: {
+        tooltips: {
+            callbacks: {
+                label: function (tooltipItem, data) {
+                    return data['labels'][tooltipItem['index']] + ': ' + data['datasets'][0]['data'][tooltipItem['index']] + '%';
+                }
+            }
+        },
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    max: 100,
+                    callback: function(value, index, values) {
+                        // Convert the number to a string and splite the string every 3 charaters from the end
+                        value = value.toString();
+                        value = value.split(/(?=(?:...)*$)/);
+            
+                        // Convert the array to a string and format the output
+                        value = value.join('.');
+                        return  value + '%';
+                    }
                 }
             }]
         }
