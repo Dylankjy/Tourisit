481e8155e933f0a2239170d99277c50dfeed1514
Dylan Kok <tenkotofu@gmail.com>
2020-12-19 15:35:30 +0800

    Added automatic retrival of IDs

--- auth.py
+++ auth.py
@@ -1,15 +1,30 @@
 import pymongo
 from bson import ObjectId
+from flask import request
 
 client = pymongo.MongoClient('mongodb+srv://admin:slapbass@cluster0.a6um0.mongodb.net/test')['Tourisit']
 db_sessions = client['Sessions']
 db_users = client['Users']
 
 
-def is_auth(req_sid):
+def get_sid():
+    try:
+        sid = request.cookies.get('sid')
+        return sid
+    except:
+        return None
+
+
+def is_auth():
+    if get_sid():
+        req_sid = get_sid()
+    else:
+        return False
+
     query = {
         "sid": req_sid
     }
+
     try:
         query_result = [i for i in db_sessions.find(query)]
 
