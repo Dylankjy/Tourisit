b714d449933d672e91d1976061d52069423ce164
Dylan Kok <tenkotofu@gmail.com>
2020-12-19 21:46:45 +0800

    Remove test file

--- main.py
+++
@@ -1,7 +0,0 @@
-import auth as auth
-
-# Valid session id, returns object from mongo
-print(auth.is_auth('superSecretSessionID'))
-
-# Invalid session id, return False
-print(auth.is_auth('hackermanGoBrrrrr'))
