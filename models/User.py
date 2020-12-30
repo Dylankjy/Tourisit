75d7b10cbc84f28cdfb9452f31fe0161a5da8c0e
CoolMysticIon <dragoamy@gmail.com>
2020-12-30 21:07:34 +0800

    Finally can update sm acc

--- models/User.py
+++ models/User.py
@@ -142,7 +142,7 @@ class User:
         self.__socialmedia.update({"fb": fb})
         self.__socialmedia.update({"insta": insta})
         self.__socialmedia.update({"linkedin": linkedin})
-        self.__socialmedia = socialmedia
+        self.__socialmedia = self.socialmedia
 
     def set_fb(self, fb):
         self.__fb = fb
