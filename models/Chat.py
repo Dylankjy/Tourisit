47505d1e44a8cdc8409bf17995dcb73b57312365
Dylan Kok <tenkotofu@gmail.com>
2021-01-01 03:19:14 +0800

    Added chat id removed receiver id
    Co-Authored-By: Chloe Low <72703739+chloryfish@users.noreply.github.com>

--- models/Chat.py
+++ models/Chat.py
@@ -1,12 +1,16 @@
 class Chat:
-    def __init__(self, sender_id, receiver_id, msg_content):
+    def __init__(self, sender_id, msg_content, is_booking_chat, chat_id):
         self.__sender_id = sender_id
-        self.__receiver_id = receiver_id
         self.__msg_content = msg_content
 
     def return_obj(self):
         return {
             "sender_id": self.__sender_id,
-            "receiver_id": self.__receiver_id,
             "msg_content": self.__msg_content,
+            "is_booking_chat": False,
+            "chat_id": self.__chat_id  
+            
+            # NEED NOT CHECK. THIS IS JUST MERELY A NOTE TO SELF.
+            # If is_booking_chat, then chat id is your booking id
+            # If NOT is_booking_chat, random string
         }
