c9f68e30ca49e42f04a421b5b53524f34df80adf
Dylan Kok <tenkotofu@gmail.com>
2021-01-02 03:41:28 +0800

    Implemented timestamping on messages

--- models/Chat.py
+++ models/Chat.py
@@ -14,10 +14,12 @@ class ChatRoom:
 class Message:  # TODO: Add timestamp!!
     def __init__(self, sender_id, timestamp, msg_content):
         self.__sender_id = sender_id
+        self.__timestamp = timestamp
         self.__msg_content = msg_content
 
     def return_obj(self):
         return {
             "sender_id": self.__sender_id,
+            "timestamp": self.__timestamp,
             "msg_content": self.__msg_content,
         }
