3fa3be54031f67ef31bd4058da6b55ca86ddcc95
Dylan Kok <tenkotofu@gmail.com>
2021-01-01 21:50:24 +0800

    Make chat into a message class which will go inside Chatroom

--- models/Chat.py
+++ models/Chat.py
@@ -1,5 +1,3 @@
-class Chat:
-    def __init__(self, sender_id, msg_content, is_booking_chat, chat_id):
         self.__sender_id = sender_id
         self.__msg_content = msg_content
 
@@ -7,10 +5,4 @@ class Chat:
         return {
             "sender_id": self.__sender_id,
             "msg_content": self.__msg_content,
-            "is_booking_chat": False,
-            "chat_id": self.__chat_id
-
-            # NEED NOT CHECK. THIS IS JUST MERELY A NOTE TO SELF.
-            # If is_booking_chat, then chat id is your booking id
-            # If NOT is_booking_chat, random string
         }
