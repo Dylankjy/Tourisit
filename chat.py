206e80b4131397479ab2d463d8bebc9b896beca8
Dylan Kok <tenkotofu@gmail.com>
2021-01-01 21:58:21 +0800

    Added debug functions & comments

--- chat.py
+++ chat.py
@@ -39,7 +39,9 @@ def create_chat_room(participants, is_booking_chat):
 
     return True
 
+
 # create_chat_room(["5feafbbf4dbad8d4b8614958", "5fec8a85b11a8931d7656f06"], True)
+# create_chat_room(["5feafbbf4dbad8d4b8614958", "5fec8a85b11a8931d7656f06"], False)
 
 
 def add_message(chat_id, sender_sid, message):
@@ -48,11 +50,13 @@ def add_message(chat_id, sender_sid, message):
         "sid": sender_sid
     }
 
+    # Find sender uid by sid
     try:
         sender_uid = [i for i in db_sessions.find(query_sid)][0]["uid"]
     except IndexError:
         return False
 
+    # Query to find specific chat user is wanting to push message into
     query_uid_in_chats = {
         '$and': [
             {
@@ -77,19 +81,31 @@ def add_message(chat_id, sender_sid, message):
         date = datetime.now()
         current_timestamp = date.isoformat()
 
+        # Create object implements Message
         chat_message_obj = Chat.Message(sender_uid, current_timestamp, message)
         chat_message_dict = chat_message_obj.return_obj()
 
+        # Query chat room by ID
         query_chat_room = {
             '_id': ObjectId(chat_id)
         }
 
+        # Payload to push into database
         payload = {
             '$push':
                 {
-                    "messages": [chat_message_dict]
+                    "messages": chat_message_dict
                 }
         }
 
+        # Database Ops: Update records with message
         db_chats.update_one(query_chat_room, payload)
+
         return True
+
+
+# test_sid = "b687c32ba5cbcde4ddb20504d832a0e7857cbff22bd6df1137097a78f0752" \
+#            "060ab64074de7acc8933c073f219fe30f62044bb1618e798e1d77703bfaf15827cd"
+
+# add_message("5fef2988e6c43266f55b00cf", test_sid, "Hello")
+# add_message("5fef29dd9816824d7f214c45", test_sid, "Hello")
