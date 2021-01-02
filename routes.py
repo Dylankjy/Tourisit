33611065c4753c5e436c12613b7ca31be59f5e37
Dylan Kok <tenkotofu@gmail.com>
2021-01-03 03:59:57 +0800

    Added jinja variable to display messages

--- routes.py
+++ routes.py
@@ -719,10 +719,8 @@ def chat():
         return redirect(url_for('login', denied_access=True))
     # if logged in
     else:
-        # TODO: Add dynamic chat to right side of page! おやすみなさい。。。ｚｚｚZZZ
         chat_list = msg.get_chat_list_for_ui(auth.get_sid(), 'BOOKING')
-        print(chat_list)
-        return render_template('chat.html', loggedin=True, user=result, list=chat_list)
+        return render_template('chat.html', loggedin=True, user=result, list=chat_list, chatroom_display=False)
 
 
 # MEMBERS
