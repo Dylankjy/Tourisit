55e95beed88a8ece6b3cd618718a6278a91d0d12
CoolMysticIon <dragoamy@gmail.com>
2021-01-10 12:22:38 +0800

    Adding profile link to fb

--- models/Support.py
+++ models/Support.py
@@ -10,27 +10,28 @@ class SupportForm(FlaskForm):
             InputRequired()
         ]
     )
-    content = TextAreaField(
-        "content",
-        validators=[
-            InputRequired(),
-            Length(min=10, max=500, message="Message can only be 500 characters long!")
-        ]
-    )
-    tg_link = StringField(
-        "tg_link",
+    link = StringField(
+        "link",
         validators=[
             InputRequired(),
             Length(min=0, max=300, message="Tour Guide profile link can only be 300 characters long!")
         ]
     )
+    content = TextAreaField(
+        "content",
+        validators=[
+            InputRequired(),
+            Length(min=0, max=500, message="Message can only be 500 characters long!")
+        ]
+    )
+
 
 class Support:
     def __init__(self,
                  uid,
                  support_type,
                  content,
-                 tg_link,
+                 link,
                  status='Open'
                  ):
         self.__uid = ''
@@ -42,8 +43,8 @@ class Support:
         self.__content = ''
         self.set_content(content)
 
-        self.__tg_link = ''
-        self.set_tg_link(tg_link)
+        self.__link = ''
+        self.set_tg_link(link)
 
         self.__status = 'Open'
         self.set_status(status)
@@ -57,8 +58,8 @@ class Support:
     def set_content(self, content):
         self.__content = content
 
-    def set_tg_link(self, tg_link):
-        self.__tg_link = tg_link
+    def set_tg_link(self, link):
+        self.__link = link
 
     def set_status(self, status):
         self.__status = status
@@ -67,6 +68,7 @@ class Support:
         return {
             "uid": self.__uid,
             "support_type": self.__support_type,
+            "link": self.__link,
             "content": self.__content,
-            "status": self.__status,
+            "status": self.__status
         }
