ea9907b930d26eab4864448bc078d8cadffbd789
Alex C <59523055+Alexc09@users.noreply.github.com>
2021-01-21 10:18:15 +0800

    u

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 apt update
 sudo apt install python3-pip
 pip3 install -r requirements.txt
+
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+
 python3 routes.py
