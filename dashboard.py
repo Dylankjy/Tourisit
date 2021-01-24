5d0d9d568a905410327acb78fa955811c35587f6
Dylan Kok <tenkotofu@gmail.com>
2021-01-24 12:53:23 +0800

    Create new index function (makes chloe's life a little more bearable)

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,8 @@ import pymongo
 # MongoDB connection string
 from bson import ObjectId
 
+import models.DashboardIndex as dindex
+
 client = pymongo.MongoClient(
     'mongodb://tourisitUser:desk-kun_did_nothing_wrong_uwu@ip.system.gov.hiy.sh:27017')['Tourisit']
 
@@ -14,4 +16,28 @@ def get_data_for_tg(uid):
         uid: ObjectId(uid)
     }
 
-    result = [i for i in db_dashboard.find(query)]
+    result = [i for i in db_dashboard.find(query)]    
+def create_index(uid):
+    """
+    Create a new index for tour guide (used when one doesn't already exist)
+    :param uid: Target user's ID
+    :return: Status of operation.
+    """
+
+    # Get entire database data
+    database_data = [i for i in db_dashboard.find({"uid": ObjectId(uid)})]
+
+    # Check whether particular user's data already exists
+    if database_data != 0:
+        return False
+
+    # Create object, implements DashboardIndex
+    index = dindex.DashboardIndexEntry(uid)
+
+    # Return object as a BSON dictionary
+    payload = index.return_obj()
+
+    # Database Ops: Insert payload into database
+    db_dashboard.insert_one(payload)
+
+    return True
